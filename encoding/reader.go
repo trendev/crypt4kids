12c7a807b94117813641a9c47284fba84e45dd17
Julien Sié <julien.sie@gmail.com>
2020-10-12 16:42:06 +0200

    refactoring encoding package

---
+++ encoding/reader.go
@@ -0,0 +1,63 @@
+package encoding
+
+import (
+	"fmt"
+	"io"
+)
+
+//Reader is a reader encoding with a specific algorithm
+type Reader struct {
+	reader io.Reader
+	fn     func(b byte) byte
+}
+
+func (r Reader) Read(p []byte) (n int, err error) {
+
+	n, err = r.reader.Read(p)
+	if err != nil {
+		if err != io.EOF { // this one is more important than EOF error
+			return 0, fmt.Errorf("can not read bytes %s : %v", p, err)
+		}
+		return n, err
+	}
+	for i := 0; i < n; i++ {
+		p[i] = r.fn(p[i])
+	}
+	return n, nil
+}
+
+//NewRot13Reader creates a new Reader using rot13 algorithm
+func NewRot13Reader(reader io.Reader) *Reader {
+	return &Reader{reader, rot13}
+}
+
+//NewAtBashReader creates a new Reader using atbash algorithm
+func NewAtBashReader(reader io.Reader) *Reader {
+	return &Reader{reader, atbash}
+}
+
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
+func atbash(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return z + a - b
+}
--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"io"
 	"os"
 
-	"github.com/trendev/crypt4kids/rot13"
+	"github.com/trendev/crypt4kids/encoding"
 )
 
 func main() {
 	fmt.Println("Enter your text and it will be translated :")
 	reader := bufio.NewReader(os.Stdin)
-	r := rot13.NewReader(reader)
+	r := encoding.NewRot13Reader(reader)
 	_, err := io.Copy(os.Stdout, r)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error copying from reader to stdout : %v\n", err)
