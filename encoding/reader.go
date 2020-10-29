ff66c8d0c8e5608f2ffa5748ec335b260171d635
Julien SIE <jsie@trendev.fr>
2020-10-29 01:04:31 +0100

    handle io.EOF

--- encoding/reader.go
+++ encoding/reader.go
@@ -14,12 +14,15 @@ type Reader struct {
 func (r Reader) Read(p []byte) (n int, err error) {
 
 	n, err = r.reader.Read(p)
-	if err != nil {
-		if err != io.EOF { // this one is more important than EOF error
-			return 0, fmt.Errorf("can not read bytes %s : %v", p, err)
-		}
-		return
+	
+	if err == io.EOF { 
+		return 
 	}
+	
+	if err != nil {
+		return n, fmt.Errorf("can not read bytes %s : %v", p, err)
+	}
+	
 	for i := 0; i < n; i++ {
 		p[i] = r.fn(p[i])
 	}
